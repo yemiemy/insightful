e4eb69a363519845d556b86c9087d6465f2157ab
Olaoluwayemi Rasheed <rasholayemi@gmail.com>
2023-11-25 10:02:11 +0000

    change applyboard

--- insight/settings.py
+++ insight/settings.py
@@ -18,7 +18,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if not DEBUG:
     import json
